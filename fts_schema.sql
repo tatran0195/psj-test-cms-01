c133a4e4e0a2e80a65aa499f00b7965b02b83947
akaw <tvta0195@gmail.com>
2026-06-15 10:46:05 +0700

    init

--- fts_schema.sql
+++
@@ -1,8 +0,0 @@
-  // The Ultimate Section-based FTS5 Engine (Trigram + Unicode61 for multi-language)
-  CREATE VIRTUAL TABLE IF NOT EXISTS blob_sections_fts USING fts5(
-    hash UNINDEXED, 
-    heading_id UNINDEXED, 
-    breadcrumb, 
-    content, 
-    tokenize="trigram"
-  );
